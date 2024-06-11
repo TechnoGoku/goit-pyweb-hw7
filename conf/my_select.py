f75b2633e5809b7a3ba9dc312bf6f0c431c25384
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-06-12 00:56:12 +0300

    update_15

--- conf/my_select.py
+++ conf/my_select.py
@@ -144,10 +144,66 @@ def select_07():
         .filter(Subject.id == 1).all()
     return result
 
+
 def select_08():
     """
-
+    SELECT
+        AVG(grades.grade) AS average_grade
+    FROM
+        grades
+    JOIN
+        subjects ON grades.subject_id = subjects.id
+    JOIN
+        teachers ON subjects.teacher_id = teachers.id
+    WHERE
+        teachers.id = 1;  -- Замените 1 на нужный id преподавателя
     """
+    result = session.query(func.avg(Grade.grade).label('average_grade')) \
+        .join(Subject, Grade.subjects_id == Subject.id) \
+        .join(Teacher, Subject.teacher_id == Teacher.id) \
+        .filter(Teacher.id == 1).scalar()
+    return result
+
+
+def select_09():
+    """
+    SELECT
+        subjects.name AS subject_name
+    FROM
+        grades
+    JOIN
+        subjects ON grades.subject_id = subjects.id
+    JOIN
+        students ON grades.student_id = students.id
+    WHERE
+        students.id = 1;  -- Замените 1 на нужный id студента
+    """
+    result = session.query(Subject.name) \
+        .join(Grade, Subject.id == Grade.subjects_id) \
+        .join(Student, Grade.student_id == Student.id) \
+        .filter(Student.id == 1).all()
+    return result
+
+
+def select_10():
+    """
+    SELECT
+        subjects.name AS subject_name
+    FROM
+        grades
+    JOIN
+        subjects ON grades.subject_id = subjects.id
+    JOIN
+        students ON grades.student_id = students.id
+    JOIN
+        teachers ON subjects.teacher_id = teachers.id
+    WHERE
+        students.id = 1  -- Замените 1 на нужный id студента
+        AND teachers.id = 2;  -- Замените 2 на нужный id преподавателя
+    """
+
+
+
 
 if __name__ == '__main__':
     print(f"query_1: {select_01()}")
@@ -163,3 +219,9 @@ if __name__ == '__main__':
     print(f"query_6: {select_06(2)}")  # введіть id групи
     print('---------------')
     print(f"query_7: {select_07()}")
+    print('---------------')
+    print(f"query_8: {select_08()}")
+    print('---------------')
+    print(f"query_9: {select_09()}")
+    print('---------------')
+    print(f"query_10: {select_10()}")
